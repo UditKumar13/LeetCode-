60e1555520c86b329c7ce277d40cd1464b498778
UditKumar13 <uditkumar2401@gmail.com>
2022-03-20 23:07:59 +0530

    matrix median

--- Questions/9.Searching and Sorting/8.Matrix median(Q binary search).java
+++ Questions/9.Searching and Sorting/8.Matrix median(Q binary search).java
@@ -1,3 +1,6 @@
+
+// Brute Force 
+TC 0(n*mlog(n*m))
 public class Solution {
     public int findMedian(int[][] A) {
 
@@ -24,4 +27,42 @@ public class Solution {
 
     }
 
+}
+
+
+// OPtimized Solution using Binary search
+
+TC O(32 * N  * log M)
+
+public class Solution {
+    private int countSmallerThanMid(ArrayList<Integer> row, int mid) {
+        int l = 0, h = row.size() - 1; 
+        while(l <= h) {
+            int md = (l + h) >> 1; 
+            if(row.get(md) <= mid) {
+                l = md + 1;
+            }
+            else {
+                h = md - 1;
+            }
+        }
+        return l; 
+    }
+    public int findMedian(ArrayList<ArrayList<Integer>> A) {
+        int low = Integer.MIN_VALUE;
+        int high = Integer.MAX_VALUE; 
+        int n = A.size();
+        int m = A.get(0).size(); 
+        while(low <= high) {
+            int mid = (low + high) >> 1; 
+            int cnt = 0;
+            for(int i = 0;i<n;i++) {
+                cnt += countSmallerThanMid(A.get(i), mid); 
+            }
+    
+            if(cnt <= (n * m) / 2) low = mid + 1; 
+            else high = mid - 1; 
+        }
+        return low; 
+    }
 }
