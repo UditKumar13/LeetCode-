968fdc4032520330a951b42a124624b9e173b1b1
UditKumar13 <uditkumar2401@gmail.com>
2022-06-05 20:35:43 +0530

    bst to gst

--- Questions/7.Trees/Binary Search Tree/19.Insert Into Binary Search Tree(Q701, med).java
+++ Questions/7.Trees/Binary Search Tree/19.Insert Into Binary Search Tree(Q701, med).java
@@ -37,3 +37,62 @@ class Solution {
 
 // SC : O(n)
 
+
+// Approach 2 : Striver 
+
+Iterative
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null) return new TreeNode(val);
+        TreeNode curr = root;
+        
+        while(true){
+            if(curr.val <= val){
+                if(curr.right !=null){
+                   curr = curr.right; 
+                }
+                
+                else{
+                    curr.right = new TreeNode(val);
+                    break;
+                }
+            }
+            
+            else{
+                if(curr.left !=null){
+                   curr = curr.left; 
+                }
+                
+                else{
+                    curr.left = new TreeNode(val);
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+}
+
+
+
+// Iterative 
+// TC : O(n)
+
+// SC: O(1)
+
