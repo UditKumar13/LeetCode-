d4f4f95464f7d71a062992c104059b2c869a9587
UditKumar13 <uditkumar2401@gmail.com>
2022-07-07 03:49:19 +0530

    leetcode 1403

--- Questions/15.UFO/msft/5.Maximum Length of  a concatenated string(Q1239,med).java
+++ Questions/15.UFO/msft/5.Maximum Length of  a concatenated string(Q1239,med).java
@@ -1,3 +1,4 @@
+// Approach 1 simple for loops and checker 
 class Solution {
     public int maxLength(List<String> arr) {
         List<String> ansList = new ArrayList<>();
@@ -30,3 +31,63 @@ class Solution {
         return true;
     }
 }
+
+// Approach 2 : Bit manipulation 
+// same time but better space optimization 
+// It helps in learning the usuage of bit, how it can save the space 
+class Solution {
+    public int maxLength(List<String> arr) {
+        // Results initialized as a Set to prevent duplicates
+        Set<Integer> results = new HashSet<>(){{add(0);}};
+        int best = 0;
+        
+        // Check each string in arr and find the best length
+        for (String word : arr) 
+            best = Math.max(best, addWord(word, results));
+        return best;
+    }
+
+    private int addWord(String word, Set<Integer> results) {
+        // Initialize an int to use as a character set
+        int charBitSet = 0;
+        int best = 0;
+        for (char c : word.toCharArray()) {
+            // Define character mask for currrent char
+            int mask = 1 << c - 'a';
+
+            // Bitwise AND check using character mask
+            // to see if char already found and if so, exit
+            if ((charBitSet & mask) > 0)
+                return 0;
+
+            // Mark char as seen in charBitSet
+            charBitSet += mask;
+        }
+    
+        // If the initial bitset is already a known result,
+        // then any possible new results will have already been found
+        if (results.contains(charBitSet))
+            return 0;
+
+        // Iterate through previous results only
+        Set<Integer> newResults = new HashSet<>();
+        for (Integer res : results) {
+            // If the two bitsets overlap, skip to the next result
+            if ((res & charBitSet) > 0)
+                continue;
+
+            // Build the new entry with bit manipulation
+            int newResLen = (res >> 26) + word.length();
+            int newCharBitSet = (charBitSet + res) & ((1 << 26) - 1);
+
+            // Merge the two into one, add it to results,
+            // and keep track of the longest so far
+            newResults.add((newResLen << 26) + newCharBitSet);
+            best = Math.max(best, newResLen);
+        }
+        results.addAll(newResults);
+        return best;
+    }
+}
+
+// using bit manipulation 
